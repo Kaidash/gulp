e1d6a8ec3b68da2a765267de133f3f2a36821616
Kaidash <thisissparta85>
2016-10-13 12:52:02 +0300

    fitst

---
+++ app/app.js
@@ -0,0 +1,5 @@
+
+'use strict';
+
+let a=123123;
+
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['js','css'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("app/sass/*.scss", ['css']);
+    gulp.watch("app/*.js", ['js']);
+
+
+    //js
+    gulp.watch("dist/**/*.js").on('change', browserSync.reload);
+    //css
+    gulp.watch("dist/css/**/*.css").on('change', browserSync.reload);
+    //HTML
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('js', function() {
+    return gulp.src('app/*.js')
+        .pipe(babel({
+            presets: ["stage-3","stage-4"]
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('css', function () {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['serve']);
